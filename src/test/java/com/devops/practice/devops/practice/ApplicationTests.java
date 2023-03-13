f894871b0ce84c9888b08ea1bd62074b5444e08c
jm_tmp <jyotsna.mishra2010@gmail.com>
2023-03-13 23:05:36 +0100

    first commit

---
+++ src/test/java/com/devops/practice/devops/practice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.practice.devops.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
